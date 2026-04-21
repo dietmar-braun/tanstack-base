32f7d353d2671b41a36d03006ac053c8f8060af6
Dietmar Braun <dbraun1985@gmail.com>
2026-04-21 18:11:23 +0200

    feat/UI; state change on click

--- apps/web/src/components/layout/navBar.tsx
+++ apps/web/src/components/layout/navBar.tsx
@@ -1,14 +1,14 @@
 import {createSignal, onMount} from 'solid-js';
 import { Link } from '@tanstack/solid-router';
-import { BiSolidHome } from 'solid-icons/bi' 
+//import { BiSolidHome } from 'solid-icons/bi' 
 import { VsHome } from 'solid-icons/vs'
 import { VsMenu } from 'solid-icons/vs'
 
  const Navbar = () => {
-const [openState, setOpenState] = createSignal(false)
+const [openState, setOpenState] = createSignal<boolean>(false)
   return (
     <nav class="flex justify-center">
-      <VsMenu  />
+      <VsMenu onClick={() => {setOpenState(!openState)}} />
       <ul class={`${openState === false ? 'hide' : ''}`}>
         <li>
           <Link to="/"><VsHome />Home</Link>
