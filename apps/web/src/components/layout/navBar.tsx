0ed1a7240e92ae2394400ed452dc6bfa69635b98
root <dbraun1985@gmail.com>
2026-04-26 15:23:24 +0200

    feat/UI: Add open & close state with action to navbar

--- apps/web/src/components/layout/navBar.tsx
+++ apps/web/src/components/layout/navBar.tsx
@@ -1,6 +1,6 @@
 import {createSignal, onMount, Show} from 'solid-js';
 import { Link } from '@tanstack/solid-router';
-
+import { VsChromeClose } from 'solid-icons/vs';
 import { VsHome } from 'solid-icons/vs'
 import { VsMenu } from 'solid-icons/vs'
 
@@ -8,16 +8,24 @@ const Navbar = () => {
   const [openState, setOpenState] = createSignal<boolean>(false)
   return (
     <nav class="flex justify-center">
-      
-      
-      <ul class={`${openState() === false ? 'hide' : ''}`}>
-        <li>
-          <Link to="/"><VsHome class="inline"/>Home</Link>
-        </li>
-      </ul>
-      
+      <div class="">
+        <ul class={`${openState() === false ? 'hide' : ''}`}>
+          <li>
+            <Link to="/"><VsHome class="inline"/>Home</Link>
+          </li>
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+          <li>
+            <Link to="/news">News</Link>
+          </li>
+        </ul>
+      </div>
       <Show when={openState() === false}>
         <VsMenu onClick={() => {setOpenState(true)}}/>
+      </Show>
+      <Show when={openState() === true}>
+        <VsChromeClose onClick={() => {setOpenState(false)}}/>
       </Show>
     </nav>
   )
