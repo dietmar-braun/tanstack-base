15b46a7ff31347242ba45bdd16139230feaf0e1a
root <dbraun1985@gmail.com>
2026-05-15 19:53:01 +0200

    feat/content: Updaye homepage with intro text

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -3,9 +3,9 @@ import { createFileRoute } from '@tanstack/solid-router'
 export const Route = createFileRoute('/')({
   component: () => {
     return (
-      <div class="">
-        <h1 class="text-4xl bg-base-100">Home</h1>
-        <p>test</p>
+      <div class="main">
+        <h1 class="text-4xl bg-base-100">Tanstack Base</h1>
+        <p>This repository is a <strong>starter bundle</strong> for projects using Tanstack with SolidJS.</p>
       </div>
     )
   }
